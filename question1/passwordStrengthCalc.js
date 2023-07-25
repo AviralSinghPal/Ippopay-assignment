2a187ddf67414fab79639cf326db5e9c99db6994
AviralSinghPal <aviralpal00007@gmail.com>
2023-07-25 21:23:47 +0530

    solved question 1 and implemented it in question 3

---
+++ question1/passwordStrengthCalc.js
@@ -0,0 +1,33 @@
+function minStepsToMakePasswordStrong(password) {
+    let steps = 0;
+    let missingConditions = 3;
+  
+    if (/[a-z]/.test(password)) {
+      missingConditions--;
+    }
+  
+    if (/[A-Z]/.test(password)) {
+      missingConditions--;
+    }
+  
+    if (/\d/.test(password)) {
+      missingConditions--;
+    }
+  
+    steps += missingConditions;
+  
+    if (password.length < 6) {
+      steps += 6 - password.length;
+    }
+  
+    for (let i = 2; i < password.length; i++) {
+      if (password[i] === password[i - 1] && password[i] === password[i - 2]) {
+        steps++;
+        break;
+      }
+    }
+  
+    return steps;
+  }
+
+  console.log((minStepsToMakePasswordStrong("Avira")));
